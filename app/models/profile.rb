c722b1fc7db6ac49899e49dd45f05ad9589a826d
Tran Van Duong <duongtvph04550@gmail.com>
2019-05-27 14:44:29 +0700

    edit-profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,16 @@
 class Profile < ApplicationRecord
+  
   belongs_to :user
+  mount_uploader :avatar, AvatarUploader 
+  validates :full_name, presence: true, on: :update
+  validates :address, presence: true, on: :update
+  validate :avatar_size, on: :update
+
+  private
+
+  def avatar_size
+      if avatar.size > 2.megabytes
+        errors.add(:avatar, "Should be less than 2MB")
+      end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  
+
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :email, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i},
             uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, presence: true
+  validates :password, presence: true, length: {minimum: 1}
 
   def remember
     self.remember_token = User.new_token
@@ -73,5 +73,6 @@ class User < ApplicationRecord
 
   has_one :profile
   delegate :full_name, to: :profile, prefix: true, allow_nil: true
-  delegate :user_avatar, to: :profile, prefix: true, allow_nil: true
+  delegate :avatar, to: :profile, prefix: true, allow_nil: true
+  delegate :address, to: :profile, prefix: true, allow_nil: true
 end
