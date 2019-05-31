181528163d8e173f262d3c9213f4062e11f0de4b
Duy Nguyen <nguyenngocduy.84@live.com>
2019-05-31 10:07:57 +0700

    Add_Tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,11 +1,25 @@
 class TagsController < ApplicationController
 
   def index
-    @tags = Tag.all.page(params[:page]).per(60)
+    @tags = Tag.all.page(params[:page]).per(160)
   end
 
   def show
     @tag = Tag.find(params[:id])
     @questions = @tag.questions.page(params[:page]).per(20)
+  end
+
+  def create
+    @tag = Tag.create tag_params
+    @tags = Tag.all.page(params[:page]).per(160)
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit :content
   end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
   has_many :question_tags
   has_many :questions, through: :question_tags
-  validates :content, length: {maximum: 16}
+  validates :content, length: {maximum: 16}, presence: true, uniqueness: true
 end
